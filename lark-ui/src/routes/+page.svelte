3790de70848c7a58d8b904a887627c58600ef0f3
Jane <yevheniiasimaka@gmail.com>
2025-11-03 11:20:00 -0700

    feat: add 404 page
    * add 404 page
    
    * fix read more button z index
    
    * add no projects case to projects page

--- lark-ui/src/routes/+page.svelte
+++ lark-ui/src/routes/+page.svelte
@@ -434,7 +434,13 @@
         </form>
       </div>
     </div>
-
+    
+    <div class="absolute bottom-8 md:bottom-12 lg:bottom-16 2xl:bottom-16 left-0 right-0 flex justify-center z-30">
+      <p class="font-['PT_Serif',_serif] font-bold text-[#fee1c0] text-lg md:text-2xl lg:text-3xl 2xl:text-[34px] text-center">
+        you receive a strange transmission... <span class="inline-block animate-bounce ml-2">⌄</span>
+      </p>
+    </div>
+    
     <div class="absolute bottom-[72px] left-0 right-0 flex justify-center z-30 md:bottom-12 md:right-12 md:left-auto lg:bottom-16 lg:right-16">
       <button 
         on:click={openModal}
@@ -445,12 +451,6 @@
           Read More
         </span>
       </button>
-    </div>
-
-    <div class="absolute bottom-8 md:bottom-12 lg:bottom-16 2xl:bottom-16 left-0 right-0 flex justify-center z-30">
-      <p class="font-['PT_Serif',_serif] font-bold text-[#fee1c0] text-lg md:text-2xl lg:text-3xl 2xl:text-[34px] text-center">
-        you receive a strange transmission... <span class="inline-block animate-bounce ml-2">⌄</span>
-      </p>
     </div>
   </section>
 
