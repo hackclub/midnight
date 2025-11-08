3ad7c76d052f32516b671fcc8c9b950cfcb283c8
techpixel <68567672+techpixel@users.noreply.github.com>
2025-11-07 20:24:35 -0500

    chore: remove unused imports

--- owl-api/src/health/health.module.ts
+++ owl-api/src/health/health.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
-import { PrismaService } from '../prisma.service';
-import { RedisService } from '../redis.service';
-import { JobLockService } from '../job-lock.service';
 
 @Module({
   imports: [],
